3d09d9dd2bd2ecd5c8566f36d754d38398c81a15
Amond00 <danielhan0717@gmail.com>
2022-11-23 12:50:54 +0900

    prl04 first commit

---
+++ pro04/src/main/java/com/ottogi/controller/MemberController.java
@@ -0,0 +1,10 @@
+package com.ottogi.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/membercon/*")
+public class MemberController {
+	
+}
